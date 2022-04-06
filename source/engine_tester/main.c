e3762d4651e12e71c5d72f0a7cc6e2e42b2725c8
Kyle Coffey <me@phytolizer.dev>
2022-04-05 21:07:55 -0500

    add Stanford dragon

--- source/engine_tester/main.c
+++ source/engine_tester/main.c
@@ -13,9 +13,9 @@ int main(void) {
     shader_program_t* shader = static_shader_new();
     renderer_t renderer = renderer_new(&display_manager, shader);
 
-    raw_model_t model = load_obj_model(DATA_ROOT_PATH "/models/stall.obj", &loader);
+    raw_model_t model = load_obj_model(DATA_ROOT_PATH "/models/dragon.obj", &loader);
     model_texture_t texture = {
-            .texture_id = loader_load_texture(&loader, "data/textures/stallTexture.png"),
+            .texture_id = loader_load_texture(&loader, "data/textures/texture.png"),
     };
     textured_model_t static_model = {
             .raw_model = model,
